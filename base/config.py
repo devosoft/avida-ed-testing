1a680bec24831cdc30a5a263f74998230ae0da65
cousi2344 <cousi2344@gmail.com>
2017-06-19 20:18:03 -0500

    Add config.py file for interfacing with a configuration file in the future.

---
+++ base/config.py
@@ -0,0 +1,7 @@
+
+
+class Configuration:
+    """
+    Class that interfaces with the main configuration file, which contains file
+    names and other configurable variables for this project.
+    """
