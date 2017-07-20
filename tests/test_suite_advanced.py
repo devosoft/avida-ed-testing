19ef7d121ee80471d770314c7fe9b2bbaa261dad
cousi2344 <cousi2344@gmail.com>
2017-07-20 13:14:58 -0500

    Same edit described in previous commit extended to test_suite_advanced

--- tests/test_suite_advanced.py
+++ tests/test_suite_advanced.py
@@ -6,13 +6,21 @@ as opposed to "basic" tests.
 """
 
 import pytest
+import sys
 
 common_adv_path = r"tests/common/common_advanced"
 pop_adv_path = r"tests/population/population_advanced"
 org_adv_path = r"tests/organism/organism_advanced"
 analysis_adv_path = r"tests/analysis/analysis_advanced"
 
-pytest.main(["-v", "-s", common_adv_path, pop_adv_path, org_adv_path, analysis_adv_path])
+pytest_args = ["-v",
+               "-s",
+               common_adv_path,
+               pop_adv_path,
+               org_adv_path,
+               analysis_adv_path]
 
+pytest_args.extend(sys.argv)
+pytest.main(pytest_args)
 
 
