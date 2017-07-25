6e9790899c073d564ed20f03dd7c983997fa2491
cousi2344 <cousi2344@gmail.com>
2017-07-25 13:10:23 -0500

    Remove unused fixtures from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
 import pytest
 from base.webdriver_factory import WebDriverFactory
-from base.base_page import BasePage
-from specializations.value_getter.value_getter import ValueGetter
-from specializations.population.population_page import PopulationPage
-from specializations.organism.organism_page import OrganismPage
-from specializations.analysis.analysis_page import AnalysisPage
 
 
 @pytest.yield_fixture(scope="class")
@@ -20,52 +15,6 @@ def driver_setup(request, browser, local, setuipath, setffpath, seturl):
     wdf.clean_webdriver_instance()
     driver.quit()
     print("\nOne time teardown begins here.")
-
-
-@pytest.yield_fixture(scope="class")
-def bp_setup(request, driver_setup):
-    bp = BasePage(driver_setup)
-
-    if request.cls is not None:
-        request.cls.bp = bp
-        request.cls.vg = ValueGetter(driver_setup)
-        request.cls.pp = PopulationPage(driver_setup)
-        request.cls.ap = AnalysisPage(driver_setup)
-
-    yield bp
-
-    assert not bp.crash_report_displayed()
-
-
-@pytest.fixture(scope="class")
-def pp_setup(request, driver_setup):
-    pp = PopulationPage(driver_setup)
-    if request.cls is not None:
-        request.cls.pp = pp
-    return pp
-
-
-@pytest.fixture(scope="class")
-def op_setup(request, driver_setup):
-    op = OrganismPage(driver_setup)
-    if request.cls is not None:
-        request.cls.op = op
-    return op
-
-
-@pytest.fixture(scope="class")
-def ap_setup(request, driver_setup) -> AnalysisPage:
-    ap = AnalysisPage(driver_setup)
-    if request.cls is not None:
-        request.cls.ap = ap
-    return ap
-
-@pytest.fixture(scope="class")
-def vg_setup(request, driver_setup):
-    vg = ValueGetter(driver_setup)
-    if request.cls is not None:
-        request.cls.vg = vg
-    return vg
 
 
 def pytest_addoption(parser):
