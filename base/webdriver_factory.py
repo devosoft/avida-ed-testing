ca39abb18424e0548c446f7dcd4fda054e2b58c0
cousi2344 <cousi2344@gmail.com>
2017-07-30 20:47:47 -0500

    Remove unused imports from webdriver_factory.py

--- base/webdriver_factory.py
+++ base/webdriver_factory.py
@@ -1,6 +1,3 @@
-import threading
-import time
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
