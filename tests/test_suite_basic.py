17f897d178d809441e475da41fdbf0b18d7dd6c9
Jacob Cousineau <cousi2344@gmail.com>
2017-08-27 10:06:11 -0400

    Add __name__="__main__" check to test_suite_basic to prevent duplicate test runs.

--- tests/test_suite_basic.py
+++ tests/test_suite_basic.py
@@ -8,20 +8,21 @@ as opposed to "advanced" tests.
 import pytest
 import sys
 
-common_basic_path = r"tests/common/common_basic"
-pop_basic_path = r"tests/population/population_basic"
-org_basic_path = r"tests/organism/organism_basic"
-analysis_basic_path = r"tests/analysis/analysis_basic"
+if __name__ == '__main__':
+    common_basic_path = r"tests/common/common_basic"
+    pop_basic_path = r"tests/population/population_basic"
+    org_basic_path = r"tests/organism/organism_basic"
+    analysis_basic_path = r"tests/analysis/analysis_basic"
 
-pytest_args = ["-v",
-               "-s",
-               "--junitxml=./output/junit_xml/junit_xml.log",
-               "--html=./output/html_report/html_report.html",
-               "--self-contained-html",
-               common_basic_path,
-               pop_basic_path,
-               org_basic_path,
-               analysis_basic_path]
+    pytest_args = ["-v",
+                   "-s",
+                   "--junitxml=./output/junit_xml/junit_xml.log",
+                   "--html=./output/html_report/html_report.html",
+                   "--self-contained-html",
+                   common_basic_path,
+                   pop_basic_path,
+                   org_basic_path,
+                   analysis_basic_path]
 
-pytest_args.extend(sys.argv)
-pytest.main(pytest_args)
+    pytest_args.extend(sys.argv)
+    pytest.main(pytest_args)
